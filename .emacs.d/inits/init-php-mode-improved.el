169d8a880d49d4a79b3844cdeecca3971a6b3251
Tetsuya Kobayashi <tetsu@nicenature.org>
2019-01-08 08:39:42 +0900

    update

--- .emacs.d/inits/init-php-mode-improved.el
+++ .emacs.d/inits/init-php-mode-improved.el
@@ -11,3 +11,14 @@
      (setq php-manual-path "/usr/local/share/php/doc/html")
      (setq php-manual-url "http://php.net/")
      ))
+(add-hook 'php-mode-hook (lambda ()
+    (defun ywb-php-lineup-arglist-intro (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (+ (current-column) c-basic-offset))))
+    (defun ywb-php-lineup-arglist-close (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (current-column))))
+    (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
+    (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)))
