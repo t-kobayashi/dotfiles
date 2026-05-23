7fb32b46045c55669d81bdc900b621a565588682
kotetsu <kotetsu@nicecode.app>
2026-05-24 06:26:45 +0900

    wezterm: tweak tab/status bar powerline symbols and title format

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- Powerline タブタイトル (色はカラースキームから動的に取得)
-local PL_LEFT  = wezterm.nerdfonts.pl_right_hard_divider
+local PL_LEFT  = wezterm.nerdfonts.ple_left_half_circle_thick
 local PL_RIGHT = wezterm.nerdfonts.pl_left_hard_divider
 local BUILTIN_SCHEMES = wezterm.color.get_builtin_schemes()
 
@@ -26,7 +26,7 @@ wezterm.on('format-tab-title', function(tab, tabs, panes, conf, hover, max_width
   local dir      = cwd and cwd.file_path or ''
   local basename = dir:match('([^/]+)/*$') or dir
   local index    = tab.tab_index + 1
-  local title    = string.format(' %d:%s ', index, basename)
+  local title    = string.format('%d %s ', index, basename)
 
   if tab.is_active then
     return {
